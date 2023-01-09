bd70dfb3cb46574ad0c855471085020fdcaf6bc3
marisofip <marisofipalencia@gmail.com>
2023-01-09 15:47:12 +0000

    subiendo tablas

--- src/models.py
+++ src/models.py
@@ -8,23 +8,46 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    correo = Column(String(250), nullable=False)
+    clave = Column(String(250), nullable=False)
+    fecha_suscripcion = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Personajes(Base):
+    __tablename__ = 'personajes'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(250), nullable=False)
+    genero = Column(String(250), nullable=False)
+    edad = Column(Integer, nullable=False)
+    nacimiento = Column(String(250), nullable=False)
+    altura = Column(Integer, nullable=False)
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    clima = Column(String(250), nullable=False)
+    diametro = Column(Integer, nullable=False)
+    terreno = Column(String(250), nullable=False)
+    poblacion = Column(Integer, nullable=False)
+
+class Likes_Personajes(Base):
+    __tablename__ = 'likes_personajes'
+    usuario_id = Column(Integer, ForeignKey('usuario.id') , primary_key=True, nullable=False)
+    personajes_id = Column(Integer,  ForeignKey('personajes.id'), primary_key=True, nullable=False)
+    
+class Likes_Planetas(Base):
+    __tablename__ = 'likes_planetas'
+    usuario_id = Column(Integer, ForeignKey('usuario.id'), primary_key=True, nullable=False )
+    planetas_id = Column(Integer, ForeignKey('planetas.id'), primary_key=True, nullable=False )
+
+
 
     def to_dict(self):
         return {}
